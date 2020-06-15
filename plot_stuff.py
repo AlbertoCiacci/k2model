63fedfb22e2954b6aa0c851faf645863ef0c3622
chester-tan <66315416+chester-tan@users.noreply.github.com>
2020-06-15 18:30:28 +0800

    Create plot_stuff.py

---
+++ plot_stuff.py
@@ -0,0 +1,20 @@
+import k2model
+
+N=10**4
+realisations=1000
+copy_prob = np.zeros(N)
+L_prob = np.zeros(N)
+for i in range(realisations):
+    G = PA_graph(n=2)
+    G.add_nodes(N)
+    copy_prob += np.array(G.copy_prob)
+    L_prob += np.array(G.L_prob)
+copy_prob /= float(realisations)
+L_prob /= float(realisations)
+plt.figure()
+plt.plot(copy_prob,'.',label='copy_prob')
+plt.plot(L_prob,'.',label='L_prob')
+# plt.xscale('log')
+# plt.yscale('log')
+plt.legend()
+plt.show()
